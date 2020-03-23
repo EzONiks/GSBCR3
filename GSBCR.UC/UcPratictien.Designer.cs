c62e12a61e00e78a264411232e768cc5279846af
Nawfel323 <Nawfel323>
2020-03-23 16:23:52 +0100

    Ajoutez des fichiers projet.

---
+++ GSBCR.UC/UcPratictien.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace GSBCR.UC
+{
+    partial class UcPratictien
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtPrenom = new System.Windows.Forms.TextBox();
+            this.txtAddr = new System.Windows.Forms.TextBox();
+            this.txtCP = new System.Windows.Forms.TextBox();
+            this.txtVille = new System.Windows.Forms.TextBox();
+            this.txtCoef = new System.Windows.Forms.TextBox();
+            this.txtFonction = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prenom";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Adresse";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Code Postal";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ville";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Coef Notoriété";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(255, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Fonction";
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(112, 45);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(122, 20);
+            this.txtNom.TabIndex = 7;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtPrenom
+            // 
+            this.txtPrenom.Location = new System.Drawing.Point(112, 97);
+            this.txtPrenom.Name = "txtPrenom";
+            this.txtPrenom.Size = new System.Drawing.Size(122, 20);
+            this.txtPrenom.TabIndex = 9;
+            // 
+            // txtAddr
+            // 
+            this.txtAddr.Location = new System.Drawing.Point(112, 148);
+            this.txtAddr.Name = "txtAddr";
+            this.txtAddr.Size = new System.Drawing.Size(191, 20);
+            this.txtAddr.TabIndex = 10;
+            // 
+            // txtCP
+            // 
+            this.txtCP.Location = new System.Drawing.Point(112, 201);
+            this.txtCP.Name = "txtCP";
+            this.txtCP.Size = new System.Drawing.Size(122, 20);
+            this.txtCP.TabIndex = 11;
+            // 
+            // txtVille
+            // 
+            this.txtVille.Location = new System.Drawing.Point(112, 249);
+            this.txtVille.Name = "txtVille";
+            this.txtVille.Size = new System.Drawing.Size(191, 20);
+            this.txtVille.TabIndex = 12;
+            // 
+            // txtCoef
+            // 
+            this.txtCoef.Location = new System.Drawing.Point(112, 294);
+            this.txtCoef.Name = "txtCoef";
+            this.txtCoef.Size = new System.Drawing.Size(122, 20);
+            this.txtCoef.TabIndex = 13;
+            // 
+            // txtFonction
+            // 
+            this.txtFonction.Location = new System.Drawing.Point(309, 45);
+            this.txtFonction.Name = "txtFonction";
+            this.txtFonction.Size = new System.Drawing.Size(193, 20);
+            this.txtFonction.TabIndex = 14;
+            // 
+            // UcPratictien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtFonction);
+            this.Controls.Add(this.txtCoef);
+            this.Controls.Add(this.txtVille);
+            this.Controls.Add(this.txtCP);
+            this.Controls.Add(this.txtAddr);
+            this.Controls.Add(this.txtPrenom);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "UcPratictien";
+            this.Size = new System.Drawing.Size(568, 356);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtPrenom;
+        private System.Windows.Forms.TextBox txtAddr;
+        private System.Windows.Forms.TextBox txtCP;
+        private System.Windows.Forms.TextBox txtVille;
+        private System.Windows.Forms.TextBox txtCoef;
+        private System.Windows.Forms.TextBox txtFonction;
+    }
+}
---
+++ GSBCR.UCTEST/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace GSBCR.UCTEST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bsMedoc = new System.Windows.Forms.BindingSource(this.components);
+            this.cbxMedoc = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxPratictien = new System.Windows.Forms.ComboBox();
+            this.bsPracticien = new System.Windows.Forms.BindingSource(this.components);
+            this.ucPratictien1 = new GSBCR.UC.UcPratictien();
+            this.ucMedicament1 = new GSBCR.UC.UcMedicament();
+            ((System.ComponentModel.ISupportInitialize)(this.bsMedoc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsPracticien)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbxMedoc
+            // 
+            this.cbxMedoc.FormattingEnabled = true;
+            this.cbxMedoc.Location = new System.Drawing.Point(15, 40);
+            this.cbxMedoc.Name = "cbxMedoc";
+            this.cbxMedoc.Size = new System.Drawing.Size(146, 21);
+            this.cbxMedoc.TabIndex = 2;
+            this.cbxMedoc.SelectedIndexChanged += new System.EventHandler(this.cbxMedoc_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Choix d\'un médicament";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(455, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Choix d\'un pratictien";
+            // 
+            // cbxPratictien
+            // 
+            this.cbxPratictien.FormattingEnabled = true;
+            this.cbxPratictien.Location = new System.Drawing.Point(458, 40);
+            this.cbxPratictien.Name = "cbxPratictien";
+            this.cbxPratictien.Size = new System.Drawing.Size(174, 21);
+            this.cbxPratictien.TabIndex = 5;
+            this.cbxPratictien.SelectedIndexChanged += new System.EventHandler(this.cbxPratictien_SelectedIndexChanged);
+            // 
+            // ucPratictien1
+            // 
+            this.ucPratictien1.Location = new System.Drawing.Point(337, 72);
+            this.ucPratictien1.Name = "ucPratictien1";
+            this.ucPratictien1.pRATICIEN = null;
+            this.ucPratictien1.Size = new System.Drawing.Size(519, 348);
+            this.ucPratictien1.TabIndex = 6;
+            // 
+            // ucMedicament1
+            // 
+            this.ucMedicament1.LeMedicament = null;
+            this.ucMedicament1.Location = new System.Drawing.Point(-28, 61);
+            this.ucMedicament1.Name = "ucMedicament1";
+            this.ucMedicament1.Size = new System.Drawing.Size(393, 359);
+            this.ucMedicament1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 421);
+            this.Controls.Add(this.ucPratictien1);
+            this.Controls.Add(this.cbxPratictien);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxMedoc);
+            this.Controls.Add(this.ucMedicament1);
+            this.Name = "Form1";
+            this.Text = "Formulaire GSBCR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bsMedoc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsPracticien)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bsMedoc;
+        private UC.UcMedicament ucMedicament1;
+        private System.Windows.Forms.ComboBox cbxMedoc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbxPratictien;
+        private UC.UcPratictien ucPratictien1;
+        private System.Windows.Forms.BindingSource bsPracticien;
+    }
+}
+
