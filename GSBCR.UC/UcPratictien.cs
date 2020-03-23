8fd6e6ee09e431ab8646e23c3689bc31fbabb621
EzONiks <paulos.ruben@gmail.com>
2020-03-23 15:47:01 +0100

    Ajoutez des fichiers projet.

---
+++ GSBCR.UC/UcPratictien.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GSBCR.modele;
+
+namespace GSBCR.UC
+{
+    public partial class UcPratictien : UserControl
+    {
+        private PRATICIEN lePracticien; 
+
+        public UcPratictien()
+        {
+            InitializeComponent();
+        }
+
+        public PRATICIEN pRATICIEN
+        {
+            get { return lePracticien; }
+            set
+            {
+                lePracticien = value;
+                if (lePracticien != null)
+                {
+                    ucPratictien_actualiser();
+                }
+            }
+        }
+
+        private void ucPratictien_actualiser()
+        {
+            txtNom.Text = lePracticien.PRA_NOM;
+            txtPrenom.Text = lePracticien.PRA_PRENOM;
+            txtAddr.Text = lePracticien.PRA_ADRESSE;
+            txtCP.Text = lePracticien.PRA_CP;
+            txtVille.Text = lePracticien.PRA_VILLE;
+            txtCoef.Text = lePracticien.PRA_COEFNOTORIETE.ToString();
+            txtFonction.Text = lePracticien.LeType.TYP_LIBELLE;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
