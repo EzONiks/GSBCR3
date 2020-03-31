d0e974b0fbece795abf7e4cbf362d470114fdc08
KagayaTV <victorravaz.pro@gmail.com>
2020-03-31 08:57:35 +0200

    sauvetage

--- UnitTestGSBCR/Properties/AssemblyInfo.cs
+++ UnitTestGSBCR/Properties/AssemblyInfo.cs
@@ -17,15 +17,6 @@ using System.Runtime.InteropServices;
 [assembly: Guid("8a33e9a5-6eb5-48bc-8ba4-b79f5d19139c")]
 
 
-
-// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
-// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
-// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
-[assembly: ComVisible(false)]
-
-// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("89698036-e77f-4450-af14-408bd4f0be57")]
-
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
 //      Version principale
