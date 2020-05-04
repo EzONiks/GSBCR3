7f87c2c05c684ab1c6f1c74ed230d74bd77e54f2
KagayaTV <victorravaz.pro@gmail.com>
2020-05-04 14:49:39 +0200

    conflitok

--- GSBCR.UI/FrmDetailPraticien.Designer.cs
+++ GSBCR.UI/FrmDetailPraticien.Designer.cs
@@ -29,78 +29,62 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDetailPraticien));
-
             this.label1 = new System.Windows.Forms.Label();
             this.btnVoirRap = new System.Windows.Forms.Button();
             this.btnQuit = new System.Windows.Forms.Button();
             this.ucPratictien1 = new GSBCR.UC.UcPratictien();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 35);
+            this.label1.TabIndex = 10;
+            // 
             // btnVoirRap
             // 
-            this.btnVoirRap.Location = new System.Drawing.Point(411, 23);
+            this.btnVoirRap.Location = new System.Drawing.Point(616, 35);
+            this.btnVoirRap.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnVoirRap.Name = "btnVoirRap";
-            this.btnVoirRap.Size = new System.Drawing.Size(149, 38);
+            this.btnVoirRap.Size = new System.Drawing.Size(224, 58);
             this.btnVoirRap.TabIndex = 8;
             this.btnVoirRap.Text = "Voir les rapports";
             this.btnVoirRap.UseVisualStyleBackColor = true;
             // 
             // btnQuit
             // 
-            this.btnQuit.Location = new System.Drawing.Point(581, 23);
+            this.btnQuit.Location = new System.Drawing.Point(616, 112);
+            this.btnQuit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnQuit.Name = "btnQuit";
-            this.btnQuit.Size = new System.Drawing.Size(149, 38);
+            this.btnQuit.Size = new System.Drawing.Size(224, 58);
             this.btnQuit.TabIndex = 9;
             this.btnQuit.Text = "Quitter";
             this.btnQuit.UseVisualStyleBackColor = true;
             this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
             // 
-<<<<<<< HEAD
-            // ucPratictien1
-=======
-            this.groupBox1.Controls.Add(this.ucPratictien1);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(429, 306);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Informations du praticien";
->>>>>>> 16a4c1298a07782676ea8a7d023386e6f23a93cb
-            // 
-            this.ucPratictien1.Location = new System.Drawing.Point(13, 69);
-            this.ucPratictien1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.ucPratictien1.Name = "ucPratictien1";
-            this.ucPratictien1.pRATICIEN = null;
-            this.ucPratictien1.Size = new System.Drawing.Size(762, 503);
-            this.ucPratictien1.TabIndex = 10;
             // ucPratictien1
             // 
             this.ucPratictien1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ucPratictien1.Location = new System.Drawing.Point(7, 24);
-            this.ucPratictien1.Margin = new System.Windows.Forms.Padding(4);
+            this.ucPratictien1.Location = new System.Drawing.Point(10, 37);
+            this.ucPratictien1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.ucPratictien1.Name = "ucPratictien1";
             this.ucPratictien1.pRATICIEN = null;
-            this.ucPratictien1.Size = new System.Drawing.Size(388, 261);
+            this.ucPratictien1.Size = new System.Drawing.Size(582, 402);
             this.ucPratictien1.TabIndex = 0;
             // 
             // FrmDetailPraticien
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-<<<<<<< HEAD
-            this.ClientSize = new System.Drawing.Size(781, 562);
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(899, 508);
             this.Controls.Add(this.ucPratictien1);
             this.Controls.Add(this.btnQuit);
             this.Controls.Add(this.btnVoirRap);
             this.Controls.Add(this.label1);
-            this.ClientSize = new System.Drawing.Size(381, 309);
-=======
-            this.BackColor = System.Drawing.Color.SkyBlue;
-            this.ClientSize = new System.Drawing.Size(453, 330);
-            this.Controls.Add(this.groupBox1);
->>>>>>> 16a4c1298a07782676ea8a7d023386e6f23a93cb
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FrmDetailPraticien";
             this.Text = "Détail d\'un praticien";
             this.Load += new System.EventHandler(this.FrmDetailPraticien_Load);
--- GSBCR.UI/FrmPraticien.Designer.cs
+++ GSBCR.UI/FrmPraticien.Designer.cs
@@ -46,20 +46,18 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(120, 35);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(180, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(259, 32);
+            this.label1.Size = new System.Drawing.Size(378, 48);
             this.label1.TabIndex = 1;
             this.label1.Text = "Liste des praticiens";
             // 
             // btnQuitter
             // 
             this.btnQuitter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.btnQuitter.Location = new System.Drawing.Point(320, 92);
-            this.btnQuitter.Margin = new System.Windows.Forms.Padding(2);
+            this.btnQuitter.Location = new System.Drawing.Point(480, 142);
             this.btnQuitter.Name = "btnQuitter";
-            this.btnQuitter.Size = new System.Drawing.Size(128, 34);
+            this.btnQuitter.Size = new System.Drawing.Size(192, 52);
             this.btnQuitter.TabIndex = 2;
             this.btnQuitter.Text = "Quitter";
             this.btnQuitter.UseVisualStyleBackColor = true;
@@ -68,20 +66,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(85, 104);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(93, 160);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(51, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "label2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(154, 104);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(235, 160);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(51, 20);
             this.label3.TabIndex = 4;
             this.label3.Text = "label3";
             // 
@@ -89,19 +85,19 @@
             // 
             this.cbPraticien.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.cbPraticien.FormattingEnabled = true;
-            this.cbPraticien.Location = new System.Drawing.Point(43, 152);
-            this.cbPraticien.Margin = new System.Windows.Forms.Padding(2);
+            this.cbPraticien.Location = new System.Drawing.Point(64, 234);
             this.cbPraticien.Name = "cbPraticien";
-            this.cbPraticien.Size = new System.Drawing.Size(237, 24);
+            this.cbPraticien.Size = new System.Drawing.Size(354, 33);
             this.cbPraticien.Sorted = true;
             this.cbPraticien.TabIndex = 7;
             this.cbPraticien.SelectedIndexChanged += new System.EventHandler(this.cbPraticien_SelectedIndexChanged);
             // 
             // btnRapport
             // 
-            this.btnRapport.Location = new System.Drawing.Point(337, 429);
+            this.btnRapport.Location = new System.Drawing.Point(506, 660);
+            this.btnRapport.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnRapport.Name = "btnRapport";
-            this.btnRapport.Size = new System.Drawing.Size(121, 30);
+            this.btnRapport.Size = new System.Drawing.Size(182, 46);
             this.btnRapport.TabIndex = 9;
             this.btnRapport.Text = "Voir les rapports";
             this.btnRapport.UseVisualStyleBackColor = true;
@@ -110,25 +106,27 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(359, 438);
+            this.label4.Location = new System.Drawing.Point(538, 674);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.Size = new System.Drawing.Size(118, 20);
             this.label4.TabIndex = 10;
             this.label4.Text = "Aucun rapports";
             // 
             // ucPratictien1
             // 
-            this.ucPratictien1.Location = new System.Drawing.Point(43, 197);
+            this.ucPratictien1.Location = new System.Drawing.Point(48, 297);
+            this.ucPratictien1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.ucPratictien1.Name = "ucPratictien1";
             this.ucPratictien1.pRATICIEN = null;
-            this.ucPratictien1.Size = new System.Drawing.Size(405, 240);
+            this.ucPratictien1.Size = new System.Drawing.Size(608, 369);
             this.ucPratictien1.TabIndex = 8;
             // 
             // FrmPraticien
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(489, 486);
+            this.ClientSize = new System.Drawing.Size(734, 748);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btnRapport);
             this.Controls.Add(this.ucPratictien1);
@@ -138,7 +136,6 @@
             this.Controls.Add(this.btnQuitter);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FrmPraticien";
             this.Text = "GSBCR";
             ((System.ComponentModel.ISupportInitialize)(this.bsPraticien)).EndInit();
